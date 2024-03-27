c8cbcb7e5f78e0628ff98533fcf013cd22a17299
k-ardliyan <k.ardliyan@gmail.com>
2024-03-27 15:43:33 +0700

    init: new project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# bookshelf-api
+# Bookshelf Api
---
+++ src/handler.js
@@ -0,0 +1,20 @@
+const { nanoid } = require('nanoid');
+const books = require('./books');
+
+const addBookHandler = (request, h) => {};
+
+const getAllBooksHandler = (request, h) => {};
+
+const getBookByIdHandler = (request, h) => {};
+
+const editBookByIdHandler = (request, h) => {};
+
+const deleteBookByIdHandler = (request, h) => {};
+
+module.exports = {
+    addBookHandler,
+    getAllBooksHandler,
+    getBookByIdHandler,
+    editBookByIdHandler,
+    deleteBookByIdHandler,
+};
